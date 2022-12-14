aa7405556e2af31473da8887a8b566d6f368d089
pronazmul <developernazmul@gmail.com>
2022-12-14 20:02:14 +0600

    Update Feature Done

--- schemas/foodSchema.js
+++ schemas/foodSchema.js
@@ -34,17 +34,36 @@ const createFoodSchema = yup.object().shape({
 })
 
 const updateFoodSchema = yup.object().shape({
-  name: yup.string().optional(),
-  typicalValueUnit: yup.string().optional(),
-  typicalValue: yup
+  water: yup.number().optional().typeError('Water Must Be Number'),
+  calories: yup.number().optional().typeError('Calories Must Be Number'),
+  protein: yup.number().optional().typeError('Protein Must Be Number'),
+  fat: yup.number().optional().typeError('Fat Must Be Number'),
+  carbohydrate: yup
     .number()
     .optional()
-    .typeError('Typical Value Must Be Number'),
-  carbs: yup.number().optional().typeError('Carbs Must Be Number'),
-  fat: yup.number().optional().typeError('Fat Must Be Number'),
-  protein: yup.number().optional().typeError('Protein Must Be Number'),
-  salt: yup.number().optional().typeError('Salt Must Be Number'),
-  sugar: yup.number().optional().typeError('Sugar Must Be Number'),
+    .typeError('Carbohydrate Must Be Number'),
+  cholesterol: yup.number().optional().typeError('Cholesterol Must Be Number'),
+  sugars: yup.number().optional().typeError('Sugars Must Be Number'),
+  caloriesFromProtein: yup
+    .number()
+    .optional()
+    .typeError('Calories From Protein Must Be Number'),
+  caloriesFromFat: yup
+    .number()
+    .optional()
+    .typeError('Calories From Fat Must Be Number'),
+  caloriesFromCarbohydrate: yup
+    .number()
+    .optional()
+    .typeError('Calories From Carbohydrate Must Be Number'),
+  energy: yup.number().optional().typeError('Energy Must be Numbers'),
+  category: yup.string().required('Category is Required!'),
+  weight: yup
+    .number()
+    .required('Weight is Required!')
+    .typeError('Weight Must Be Number'),
+  unit: yup.string().required('Recipe Unit is Required!'),
+  name: yup.string().required('Recipe name is Required!'),
 })
 
 module.exports = {
